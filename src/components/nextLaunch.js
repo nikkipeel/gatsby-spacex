488012f6b029d595738ea5e98d49a5ba0050e672
nikkipeel <npeel3497@gmail.com>
2021-03-25 09:28:22 -0400

    add nextLaunch.js

--- src/components/nextLaunch.js
+++ src/components/nextLaunch.js
@@ -16,7 +16,13 @@ const NextLaunch = () => {
   }, [])
 
   if (launch === null) {
-    return <p>Loading next launch...</p>
+    return <div class="spinner-box">
+    <div class="pulse-container">  
+      <div class="pulse-bubble pulse-bubble-1"></div>
+      <div class="pulse-bubble pulse-bubble-2"></div>
+      <div class="pulse-bubble pulse-bubble-3"></div>
+    </div>
+  </div>
   }
 
   return (
